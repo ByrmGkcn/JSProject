937305e5790fe3caae319466760f5667578bdce3
ByrmGkcn <bayram.gokcen@etu.unilim.fr>
2024-03-03 22:31:07 +0100

    RabitMQ fait!

--- server/consumer.js
+++ server/consumer.js
@@ -1,32 +1,22 @@
 'use strict';
 
 const Amqp = require('amqplib');
-const MailService = require('../lib/services/mail');
+const MailService = require('../lib/services/emailService');
 
 const receiveCsvFromQueue = async () => {
     const queue = 'movie-export';
 
     try {
-        console.log('Attempting to connect to RabbitMQ...');
 
         const connection = await Amqp.connect('amqp://localhost');
-        console.log('Connected to RabbitMQ.');
 
         const channel = await connection.createChannel();
-        console.log('Channel created.');
 
-        console.log(`Asserting queue: ${queue}`);
         await channel.assertQueue(queue, { durable: false });
-        console.log(`Queue asserted: ${queue}`);
 
-        // Recevoir les données CSV de la file d'attente
-        console.log(' [*] Waiting for CSV data...');
         channel.consume(queue, (msg) => {
             const csvData = msg.content.toString();
-            console.log(' [x] Received CSV data:', csvData);
 
-            // Envoyer les données CSV par e-mail
-            console.log('Sending CSV data by email...');
             sendCsvByEmail(csvData);
         }, { noAck: true });
     } catch (error) {
@@ -37,12 +27,8 @@ const receiveCsvFromQueue = async () => {
 
 
 const  sendCsvByEmail = (csvData) => {
-    // Envoyer les données CSV par e-mail en utilisant le service MailService
     const mailService = new MailService();
-    console.log('Mail service created.');
-    mailService.sendEmailWithAttachment('admin@gmail.com', 'Movies CSV', 'Please find attached the movies CSV file.', csvData).then((r) => console.log(r)).catch((e) => console.error(e));
-
-    console.log(' [x] Sent CSV data by email');
+    mailService.sendEmailWithAttachment('noreply@noreply.com', 'Movies CSV', 'Please find attached the movies CSV file.', csvData);
 };
 
 module.exports = { receiveCsvFromQueue };
